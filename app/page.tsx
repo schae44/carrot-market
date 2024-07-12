b13cb9fb593c6cf9b42934f567521d9b24797670
Sarah <sarahchae0219@gmail.com>
2024-07-12 11:32:48 -0500

    hands-on directives in tailwindCSS

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ export default function Home() {
     <main className="bg-gray-300 h-screen flex items-center justify-center text-black p-5">
       <div className="bg-white w-full p-5 shadow-lg rounded-2xl max-w-screen-sm flex flex-col gap-4">
         {["Nico", "Me", "You", "Sarah"].map((person, index) => (
-          <div key={index} className="flex items-center gap-3">
-            <div className="size-10 bg-blue-400 rounded-full" />
+          <div key={index} className="group flex items-center gap-3">
+            <div className="profile" />
             <span className="text-lg font-medium">{person}</span>
             <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full">
               <span className="z-10">{index}</span>
