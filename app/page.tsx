99448c593121e5bb4e39cb1130a977cabe18fe42
Sarah <sarahchae0219@gmail.com>
2024-07-12 10:46:05 -0500

    tailwindCSS modifiers practice

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,19 @@
 export default function Home() {
   return (
     <main className="bg-gray-300 h-screen flex items-center justify-center text-black p-5">
-      <div className="bg-white w-full p-5 shadow-lg rounded-2xl">
-        <div className="flex justify-between items-center">
-          <div className="flex flex-col">
-            <span className="text-gray-600 font-semibold -mb-1">
-              In transit
-            </span>
-            <span className="text-4xl font-semibold">Cool blue</span>
-          </div>
-          <div className="size-12 bg-orange-400 rounded-full" />
-        </div>
-        <div className="my-2 flex items-center gap-2">
-          <span className="bg-green-400 text-white uppercase px-2.5 py-1.5 text-xs font-medium rounded-full">
-            Today
-          </span>
-          <span>9:30-10:30</span>
-        </div>
-        <div className="relative">
-          <div className="absolute bg-gray-200 rounded-full w-full h-2" />
-          <div className="absolute bg-green-400 rounded-full w-2/3 h-2" />
-        </div>
-        <div className="flex justify-between items-center mt-5 text-gray-600">
-          <span>Expected</span>
-          <span>Sorting Center</span>
-          <span>In transit</span>
-          <span className="text-gray-400">Delivered</span>
-        </div>
+      <div className="bg-white w-full p-5 shadow-lg rounded-2xl max-w-screen-sm flex flex-col gap-2 md:flex-row *:outline-none ring ring-transparent transition-shadow has-[:invalid]:ring-red-100">
+        <input
+          className="peer w-full rounded-full h-10 bg-gray-200 pl-5 ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500"
+          type="text"
+          placeholder="Email address"
+          required
+        />
+        <span className="text-red-500 font-medium hidden peer-invalid:block">
+          Email is required
+        </span>
+        <button className="bg-black text-white py-2 rounded-full active:scale-90 transition-transform font-medium md:px-10">
+          Log in
+        </button>
       </div>
     </main>
   );
