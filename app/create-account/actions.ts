e355a4570a84f32152f0fd9ce26c56d1ba6a56a1
Sarah <sarahchae0219@gmail.com>
2024-07-15 11:34:00 -0500

    Add custum errors on validation

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -2,11 +2,24 @@
 import {z} from "zod";
 
 const formSchema = z.object({
-    username: z.string().min(3).max(10),
+    username: z.string({
+        invalid_type_error: "Username must be a string",
+        required_error: "Please enter your username"
+    }).min(3, "Username should be at least 3 characters")
+      .max(10, "Username should be at most 10 characters")
+      .refine((username)=> !(username.includes("potato")), "No potatoes allowed"),
     email: z.string().email(),
     password: z.string().min(10),
     confirm_password: z.string().min(10),
-})
+}).superRefine(({ password, confirm_password }, ctx) => {
+    if (password !== confirm_password) {
+      ctx.addIssue({
+        code: "custom",
+        message: "Two passwords should be equal",
+        path: ["confirm_password"],
+      });
+    }
+  });
 
 export async function createAccount(prevState:any, formData:FormData){
     const data ={
