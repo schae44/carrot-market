2571b65f801a01f7a37e31cd5d3653b8beb68598
Sarah <sarahchae0219@gmail.com>
2024-07-16 13:29:39 -0500

    Add SMS validation

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -1,8 +1,40 @@
 "use server";
 
-export async function smsVerification(prevState: any, formData: FormData) {
-    const data = {
-        phone: formData.get("phone"),
-        token: formData.get("token")
+import { z } from "zod";
+import validator from "validator";
+import { redirect } from "next/navigation";
+
+const phoneSchema = z.string().trim().refine(validator.isMobilePhone, "Wrong phone format");
+const tokenSchema = z.coerce.number().min(100000).max(999999)
+
+interface ActionState {
+    token: boolean
+}
+
+export async function smsLogin(prevState: ActionState, formData: FormData) {
+    const phone = formData.get("phone")
+    const token=  formData.get("token")
+    if (!prevState.token) {
+        const result = phoneSchema.safeParse(phone)
+        if(!result.success) {
+           return {
+                token: false,
+                error: result.error.flatten()
+            }
+        } else {
+            return {
+                token: true
+            }
+        }
+    } else {
+        const result = tokenSchema.safeParse(token)
+        if(!result.success){
+            return {
+                token: true,
+                error: result.error.flatten()
+            }
+        } else {
+            redirect("/");
+        }
     }
 }
