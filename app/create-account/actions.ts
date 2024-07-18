973d628b1dd2e53be05d69e24dc271b51c03fb8a
Sarah <sarahchae0219@gmail.com>
2024-07-18 11:52:42 -0500

    Implement Prisma Client

---
+++ app/create-account/actions.ts
@@ -0,0 +1,39 @@
+"use server";
+import { PASSWORD_MIN_LENGTH, PASSWORD_REGEX, PASSWORD_REGEX_ERROR } from "@/lib/constants";
+import {z} from "zod";
+
+const formSchema = z.object({
+    username: z.string({
+        invalid_type_error: "Username must be a string",
+        required_error: "Please enter your username"
+    })
+      .toLowerCase()
+      .trim()
+      .refine((username)=> !(username.includes("potato")), "No potatoes allowed"),
+    email: z.string().email().toLowerCase(),
+    password: z.string().min(PASSWORD_MIN_LENGTH).regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
+    confirm_password: z.string().min(PASSWORD_MIN_LENGTH),
+}).superRefine(({ password, confirm_password }, ctx) => {
+    if (password !== confirm_password) {
+      ctx.addIssue({
+        code: "custom",
+        message: "Two passwords should be equal",
+        path: ["confirm_password"],
+      });
+    }
+  });
+
+export async function createAccount(prevState:any, formData:FormData){
+    const data ={
+        username: formData.get("username"),
+        email: formData.get("email"),
+        password: formData.get("password"),
+        confirm_password: formData.get("confirm_password"),
+    };
+    const result = formSchema.safeParse(data);
+    if(!result.success) {
+        return result.error.flatten();
+    } else {
+      console.log(result.data)
+    }
+}
---
+++ app/login/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import FormButton from "@/components/button";
+import FormInput from "@/components/input";
+import SocialLogin from "@/components/social-login";
+import { useFormState } from "react-dom";
+import { login } from "./actions";
+import { PASSWORD_MIN_LENGTH } from "@/lib/constants";
+
+export default function LogIn() {
+  const [state, action] = useFormState(login, null);
+  return (
+    <div className="flex flex-col gap-10 py-8 px-6">
+      <div className="flex flex-col gap-2 *:font-medium">
+        <h1 className="text-2xl">Hi!</h1>
+        <h2 className="text-xl">Log in with email and password</h2>
+      </div>
+      <form action={action} className="flex flex-col gap-3">
+        <FormInput
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          errors={state?.fieldErrors.email}
+        />
+        <FormInput
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          minLength={PASSWORD_MIN_LENGTH}
+          errors={state?.fieldErrors.password}
+        />
+        <FormButton text="Log in" />
+      </form>
+      <SocialLogin />
+    </div>
+  );
+}
