ac6ef6c528d176196e844fd806270466f932a7e1
Sarah <sarahchae0219@gmail.com>
2024-07-29 11:28:02 -0500

    password hash implemented

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,46 +1,60 @@
 "use server";
-import { PASSWORD_MIN_LENGTH, PASSWORD_REGEX, PASSWORD_REGEX_ERROR } from "@/lib/constants";
+import {
+  PASSWORD_MIN_LENGTH,
+  PASSWORD_REGEX,
+  PASSWORD_REGEX_ERROR,
+} from "@/lib/constants";
 import db from "@/lib/db";
-import {z} from "zod";
+import { z } from "zod";
+import bcrypt from "bcrypt";
 
 const checkUniqueUsername = async (username: string) => {
   const user = await db.user.findUnique({
-        where: {
-          username
-        },
-        select: {
-          id: true
-        }
-      })
-  return !Boolean(user)
-}
+    where: {
+      username,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return !Boolean(user);
+};
 
 const checkUniqueEmail = async (email: string) => {
   const userEmail = await db.user.findUnique({
-        where: {
-          email
-        },
-        select: {
-          id: true
-        }
-      })
-  return !Boolean(userEmail)
-}
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return !Boolean(userEmail);
+};
 
-const formSchema = z.object({
-    username: z.string({
+const formSchema = z
+  .object({
+    username: z
+      .string({
         invalid_type_error: "Username must be a string",
-        required_error: "Please enter your username"
-    })
+        required_error: "Please enter your username",
+      })
       .toLowerCase()
       .trim()
-      .refine(checkUniqueUsername, "This username is already taken")
-      ,
-      //.refine((username)=> !(username.includes("potato")), "No potatoes allowed"),
-    email: z.string().email().toLowerCase().refine(checkUniqueEmail, "There is an account already register with that email"),
-    password: z.string().min(PASSWORD_MIN_LENGTH),//.regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
+      .refine(checkUniqueUsername, "This username is already taken"),
+    //.refine((username)=> !(username.includes("potato")), "No potatoes allowed"),
+    email: z
+      .string()
+      .email()
+      .toLowerCase()
+      .refine(
+        checkUniqueEmail,
+        "There is an account already register with that email"
+      ),
+    password: z.string().min(PASSWORD_MIN_LENGTH), //.regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
     confirm_password: z.string().min(PASSWORD_MIN_LENGTH),
-}).superRefine(({ password, confirm_password }, ctx) => {
+  })
+  .superRefine(({ password, confirm_password }, ctx) => {
     if (password !== confirm_password) {
       ctx.addIssue({
         code: "custom",
@@ -50,23 +64,30 @@ const formSchema = z.object({
     }
   });
 
-export async function createAccount(prevState:any, formData:FormData){
-    const data ={
-        username: formData.get("username"),
-        email: formData.get("email"),
-        password: formData.get("password"),
-        confirm_password: formData.get("confirm_password"),
-    };
-    const result = await formSchema.safeParseAsync(data);
-    if(!result.success) {
-        return result.error.flatten();
-    } else {
-      // hash password
-
-      // save user into db
-
-      // log the user in 
-
-      // redirect the user to "/home"
-    }
+export async function createAccount(prevState: any, formData: FormData) {
+  const data = {
+    username: formData.get("username"),
+    email: formData.get("email"),
+    password: formData.get("password"),
+    confirm_password: formData.get("confirm_password"),
+  };
+  const result = await formSchema.safeParseAsync(data);
+  if (!result.success) {
+    return result.error.flatten();
+  } else {
+    const hashedPassword = await bcrypt.hash(result.data.password, 12);
+    const user = await db.user.create({
+      data: {
+        username: result.data.username,
+        email: result.data.email,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+      },
+    });
+    console.log(user);
+    // log the user in
+    // redirect the user to "/home"
+  }
 }
