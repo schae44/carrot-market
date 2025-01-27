80871bc46c0eebd41c9ab7098df2db5a1c0574dc
Sarah <sarahchae0219@gmail.com>
2025-01-27 14:28:03 -0500

    package updated

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -7,6 +7,9 @@ import {
 import db from "@/lib/db";
 import { z } from "zod";
 import bcrypt from "bcrypt";
+import { getIronSession } from "iron-session";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 const checkUniqueUsername = async (username: string) => {
   const user = await db.user.findUnique({
@@ -86,8 +89,13 @@ export async function createAccount(prevState: any, formData: FormData) {
         id: true,
       },
     });
-    console.log(user);
-    // log the user in
-    // redirect the user to "/home"
+    const cookie = await getIronSession(cookies(), {
+      cookieName: "delicious-karrot",
+      password: process.env.COOKIE_PASSWORD!,
+    });
+    //@ts-ignore
+    cookie.id = user.id;
+    await cookie.save();
+    redirect("/profile");
   }
 }
